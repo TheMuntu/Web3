75d9e9e02e5f77b0376172f084ea84c2bd7cbaff
Pacome Kemkeu <59522058+TheMuntu@users.noreply.github.com>
2024-07-08 11:21:12 +0200

    Update SimpleStorage.sol

--- Smart contract development with Solidity/SimpleStorage.sol
+++ Smart contract development with Solidity/SimpleStorage.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;  //solidity version
+
+contract SimpleStorage {
+
+    uint256 public favoriteNumber; // 0
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+}
+
 
