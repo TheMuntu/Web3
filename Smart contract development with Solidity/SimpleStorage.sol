337035ae4ebb8f964bd026874454e5a184db91ec
Pacome Kemkeu <59522058+TheMuntu@users.noreply.github.com>
2024-07-09 14:53:52 +0200

    Update SimpleStorage.sol

--- Smart contract development with Solidity/SimpleStorage.sol
+++ Smart contract development with Solidity/SimpleStorage.sol
@@ -3,16 +3,29 @@ pragma solidity 0.8.18;  //solidity version
 
 contract SimpleStorage {
 
-    uint256 public favoriteNumber; // 0
+    uint256 public myFavoriteNumber; // 0
+
+    // uint256[] listOfFavoriteNumbers;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // dynamic array
+    Person[] public listOfPeople;
+    // Person public john = Person({favoriteNumber: 7, name: "John"});
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
-
 
     function retrieve() public view returns(uint256){
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
-}
 
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push( Person(_favoriteNumber, _name));
+    }
+
+}
 
