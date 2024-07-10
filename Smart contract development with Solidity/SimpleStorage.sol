bc2e7897bbf6fc314ba718dce53cedf383334522
Pacome Kemkeu <59522058+TheMuntu@users.noreply.github.com>
2024-07-10 09:43:26 +0200

    Update SimpleStorage.sol

--- Smart contract development with Solidity/SimpleStorage.sol
+++ Smart contract development with Solidity/SimpleStorage.sol
@@ -17,7 +17,7 @@ contract SimpleStorage {
 
     mapping (string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public virtual {
         myFavoriteNumber = _favoriteNumber;
     }
 
