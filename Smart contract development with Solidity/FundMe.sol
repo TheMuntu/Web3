e8edf7c43c477e93d0a81ef8ada04f6b94f0faf7
Pacome Kemkeu <59522058+TheMuntu@users.noreply.github.com>
2024-07-12 11:27:30 +0200

    Update FundMe.sol

--- Smart contract development with Solidity/FundMe.sol
+++ Smart contract development with Solidity/FundMe.sol
@@ -4,19 +4,29 @@ pragma solidity 0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
+
+    uint256 minimumUsd = 5e18;
     function fund() public payable {
 
-        require(msg.value > 1e18, "didn't send enough eth");
+        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough eth");
     }
 
-    function getPrice() public {
+    function getPrice() public view returns(uint256) {
         // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         // ABI 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        // Price of ETH in terms of USD
+        return uint256(price * 1e10);
     }
 
     function getVersion() public view returns (uint256){
         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
         
     }
-    function getConversionRate() public {}
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 }
