7e0ce0affa43997e1a46b8a6b8bf7efa5f136790
Pacome Kemkeu <59522058+TheMuntu@users.noreply.github.com>
2024-07-12 11:02:52 +0200

    Update FundMe.sol

--- Smart contract development with Solidity/FundMe.sol
+++ Smart contract development with Solidity/FundMe.sol
@@ -1,1 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+// solhint-disable-next-line interface-starts-with-i
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
+contract FundMe{
+    function fund() public payable {
+
+        require(msg.value > 1e18, "didn't send enough eth");
+    }
+
+    function getPrice() public {
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI 
+    }
+
+    function getVersion() public view returns (uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+        
+    }
+    function getConversionRate() public {}
+}
