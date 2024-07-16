8323224f633d474af88eafc88d36e9977a93155e
Pacome Kemkeu <59522058+TheMuntu@users.noreply.github.com>
2024-07-16 23:10:10 +0200

    Removed SimpleStorage contract

--- Smart contract development with Solidity/StorageFactory.sol
+++ Smart contract development with Solidity/StorageFactory.sol
@@ -2,37 +2,6 @@
 pragma solidity 0.8.18;  //solidity version
 import {SimpleStorage} from "./SimpleStorage.sol";
 
-/**
-contract SimpleStorage {
-
-    uint256 public myFavoriteNumber; // 0
- 
-    // uint256[] listOfFavoriteNumbers;
-    struct Person {
-        uint256 favoriteNumber;
-        string name;
-    }
-
-    // dynamic array
-    Person[] public listOfPeople;
-
-    mapping (string => uint256) public nameToFavoriteNumber;
-
-    function store(uint256 _favoriteNumber) public {
-        myFavoriteNumber = _favoriteNumber;
-    }
-
-    function retrieve() public view returns(uint256){
-        return myFavoriteNumber;
-    }
-
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push( Person(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
-
-} **/
-
 
 contract StorageFactory{
 
