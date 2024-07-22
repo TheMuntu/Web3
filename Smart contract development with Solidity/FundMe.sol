92f81ba9e33801b07060c272d35ba558150ec534
Pacome Kemkeu <59522058+TheMuntu@users.noreply.github.com>
2024-07-22 16:39:39 +0200

    Update FundMe.sol

--- Smart contract development with Solidity/FundMe.sol
+++ Smart contract development with Solidity/FundMe.sol
@@ -33,4 +33,32 @@ contract FundMe{
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUsd;
     }
+
+    function withdraw() public {
+        // for loop
+        // [1, 2, 3, 4] elements
+        // 0, 1, 2, 3 indexes
+        // for(/* starting index, ending index, step amount */ )
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) 
+        {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // reset the array
+
+        funders = new address[](0);
+        // actually withdraw the funds
+
+        // transfer
+        payable(msg.sender).transfer(address(this).balance);
+        // send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+        
+        
+    }
+
 }
