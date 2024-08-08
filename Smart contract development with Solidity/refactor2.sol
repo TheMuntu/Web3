ac2ad0fa2788291ca4353a48a080656a772a47fb
Pacome Kemkeu <59522058+TheMuntu@users.noreply.github.com>
2024-08-09 00:35:08 +0200

    Update refactor2.sol

--- Smart contract development with Solidity/refactor2.sol
+++ Smart contract development with Solidity/refactor2.sol
@@ -1,2 +1,10 @@
-// No License
-// Refactoring this code
+// SPDX-License-Identifier: MIT
+// 1. Pragma
+pragma solidity 0.8.19;
+// 2. Imports
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+// 3. Interfaces, Libraries, Contracts
+error FundMe__NotOwner();
